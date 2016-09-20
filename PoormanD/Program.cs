3e330eb9e15f4bba6914510b304a44468605fdad
supermomonga <hi@supermomonga.com>
2016-09-20 14:36:41 +0900

    move codes to MainAsync method

--- PoormanD/Program.cs
+++ PoormanD/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Net;
 using System.Threading.Tasks;
 using System.Net.Sockets;
@@ -12,23 +13,29 @@ namespace PoormanD
     {
         public static void Main(string[] args)
         {
-            // TODO: getopt
-            const int port = 1234;
+            MainAsync(args).GetAwaiter().GetResult();
+        }
 
-            var ipAddress = Dns.Resolve(Dns.GetHostName()).AddressList[0];
-            var ipLocalEndPoint = new IPEndPoint(ipAddress, port);
+        static async Task MainAsync(string[] args)
+        {
+            // TODO: getopt from args
+            const int port = 1234;
+            const string host = "localhost";
 
             try
             {
+                // Get IP address from hostname and listen it.
+                var ipAddresses = await Dns.GetHostAddressesAsync(host);
+                var ipLocalEndPoint = new IPEndPoint(ipAddresses[1], port);
                 var listener = new TcpListener(ipLocalEndPoint);
+                listener.Start();
+                Console.WriteLine("Listen on {0}:{1}", ipLocalEndPoint.Address, ipLocalEndPoint.Port);
             }
             catch (Exception e)
             {
+                Console.WriteLine("Failed to open TCP socket listener.");
                 Console.WriteLine(e.ToString());
             }
-
-            listener.Start();
-            Console.WriteLine("Listen on {0}:{1}", ipLocalEndpoint.Address, ipLocalEndpoint.Port);
         }
     }
 }
